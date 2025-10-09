8632d2d35a7c9a39108363967bff3c61210a2908
Jae Won Choi <choijaewon959@gmail.com>
2025-10-09 14:12:48 -0500

    update main

---
+++ A3/main.py
@@ -0,0 +1,20 @@
+
+
+
+from data_loader import load_data
+
+
+
+def main():
+    # 1. load data
+    data_points = load_data() # tick data points
+
+    # 2. initialize strategy
+
+    # 3. profile strategy
+
+    # 4. output results
+
+
+if __name__ == "__main__":
+    main()
