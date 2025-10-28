cce91427f169cf57c2191c00cde961c8a50d6995
Jae Won Choi <choijaewon959@gmail.com>
2025-10-27 22:46:42 -0500

    update data loader

--- A7/data_loader.py
+++ A7/data_loader.py
@@ -1,24 +1,40 @@
+import time
 import pandas as pd
 import polars as pl
-
+from memory_profiler import memory_usage
 
 def load_data_pandas(file_path: str) -> pd.DataFrame:
+    start = time.perf_counter()
     df = pd.read_csv(file_path, parse_dates=True).set_index('timestamp', drop=False)
-    return df
+    end = time.perf_counter()
+
+    elapsed_time = end - start
+    mem = memory_usage((pd.read_csv, (file_path,)), max_usage=True)    
+    return df, elapsed_time, mem
 
 def load_data_polars(file_path: str) -> pl.DataFrame:
+    start = time.perf_counter()
     df = (
         pl.read_csv(file_path, has_header=True, try_parse_dates=True)
           .sort("timestamp")
           .with_columns(pl.col("timestamp").alias("_index"))
     )
-    return df
+    end = time.perf_counter()
+
+    elapsed_time = end - start
+    mem = memory_usage((pl.read_csv, (file_path,)), max_usage=True)
+
+    return df, elapsed_time, mem
 
 if __name__ == "__main__":
-    pandas_df = load_data_pandas("./data/market_data-1.csv")
+    pandas_df, pandas_time, pandas_mem = load_data_pandas("./data/market_data-1.csv")
     print("Pandas DataFrame:")
     print(pandas_df.head())
+    print(f"Elapsed Time: {pandas_time:.4f} seconds")
+    print(f"Memory Usage: {pandas_mem} MiB")
 
-    polars_df = load_data_polars("./data/market_data-1.csv")
+    polars_df, polars_time, polars_mem = load_data_polars("./data/market_data-1.csv")
     print("\nPolars DataFrame:")
     print(polars_df.head())
+    print(f"Elapsed Time: {polars_time:.4f} seconds")
+    print(f"Memory Usage: {polars_mem} MiB")
