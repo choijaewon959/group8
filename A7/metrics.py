c98bcce3074e18db3b2cd133db350a2f56f93eb3
Jae Won Choi <choijaewon959@gmail.com>
2025-10-28 23:29:58 -0500

    add parallel

--- A7/metrics.py
+++ A7/metrics.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import polars as pl
 from data_loader import load_data_pandas, load_data_polars
+from reporting import plot_rolling_metrics
 
 
-def compute_rolling_metrics_pandas(df: pd.DataFrame, symbol: str, ts_cols: list, window=20) -> tuple[pd.DataFrame, float]:
+def rolling_metrics_pandas(df: pd.DataFrame, symbol: str, ts_cols: list, window=20) -> tuple[pd.DataFrame, float]:
     df_symbol = df[df["symbol"] == symbol].copy()
     df_symbol = df_symbol.sort_values("timestamp")
 
@@ -33,7 +34,7 @@ def compute_rolling_metrics_pandas(df: pd.DataFrame, symbol: str, ts_cols: list,
     return df_symbol
 
 
-def compute_rolling_metrics_polars(df: pl.DataFrame, symbol: str, ts_cols: list, window: int = 20) -> tuple[pl.DataFrame, float]:
+def rolling_metrics_polars(df: pl.DataFrame, symbol: str, ts_cols: list, window: int = 20) -> tuple[pl.DataFrame, float]:
     df_symbol = df.filter(pl.col("symbol") == symbol).sort("timestamp")
     df_symbol = (
         df.filter(pl.col("symbol") == symbol)
@@ -63,6 +64,7 @@ def compute_rolling_metrics_polars(df: pl.DataFrame, symbol: str, ts_cols: list,
     return df_symbol
 
 
+
 if __name__ == "__main__":
     symbol = 'AAPL'
     window = 20
@@ -70,7 +72,9 @@ if __name__ == "__main__":
     file_path = "./data/market_data-1.csv"
 
     df_pandas, _, _ = load_data_pandas(file_path)
-    df_pandas_metrics = compute_rolling_metrics_pandas(df_pandas, symbol, ['price'], window=window)
+    df_pandas_metrics = rolling_metrics_pandas(df_pandas, symbol, ['price'], window=window)
+    plot_rolling_metrics(df_pandas_metrics, window=window, subsample_size=subsample_size)
 
     df_polars, _, _ = load_data_polars(file_path)
-    df_polars_metrics = compute_rolling_metrics_polars(df_polars, symbol, ['price'], window=window)
+    df_polars_metrics = rolling_metrics_polars(df_polars, symbol, ['price'], window=window)
+    plot_rolling_metrics(df_polars_metrics, window=window, subsample_size=subsample_size)
