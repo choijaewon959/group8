bc7830b2be9c5e8219eee2e9ff623cbc28dd4bff
Jae Won Choi <choijaewon959@gmail.com>
2025-11-04 17:37:08 -0600

    toy example for startegy

--- A8/main.py
+++ A8/main.py
@@ -1,14 +1,19 @@
 from multiprocessing import Process
 from gateway import start_server
 from orderbook import start_orderbook
+from strategy import start_strategy
 
 def run_gateway():
     print("[Gateway] Starting feed server...")
     start_server()
 
 def run_orderbook():
-    print("[Orderbook] Starting orderbook client...")
+    print("[ORDERBOOK] Starting orderbook client...")
     start_orderbook()
+
+def run_strategy():
+    print("[STRATEGY] Starting strategy client...")
+    start_strategy()
 
 
 
@@ -17,7 +22,7 @@ if __name__ == "__main__":
     processes = [
         Process(target=run_gateway),
         Process(target=run_orderbook),
-        # Process(target=run_strategy),
+        Process(target=run_strategy),
         # Process(target=run_ordermanager)
     ]
     for p in processes: p.start()
---
+++ A8/strategy.py
@@ -0,0 +1,28 @@
+import socket
+
+
+def start_strategy():
+    client = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM
+    )
+
+    client.connect(("localhost", 8999))
+    client.sendall(b"REGISTER,STRATEGY,1*")
+    counter = 0
+    len_nums = 100
+    total = 0
+
+    while counter < len_nums:
+        res = client.recv(1024)
+        if not res:
+            break
+        print("[STRATEGY]", res)
+        # print(res.decode())
+        
+    # client.sendall(f"{total/counter}".encode())
+    client.close()
+
+
+if __name__ == "__main__":
+    start_strategy()
