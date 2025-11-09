7856fc95773ec40cb8728b2791a30a286b44e295
Jae Won Choi <choijaewon959@gmail.com>
2025-11-08 19:23:43 -0600

    adding order manager run

--- A8/main.py
+++ A8/main.py
@@ -2,6 +2,7 @@ from multiprocessing import Process
 from time import sleep
 from gateway import start_server
 from orderbook import start_orderbook
+from order_manager import start_ordermanager
 from strategy import start_news_strategy, start_price_strategy
 
 def run_gateway():
@@ -23,6 +24,11 @@ def run_news_strategy():
     print("[STRATEGY] Starting news strategy...")
     start_news_strategy()
 
+def run_ordermanager():
+    sleep(3)
+    print("[ORDER MANAGER] Starting order manager...")
+    start_ordermanager()
+
 
 if __name__ == "__main__":
     processes = [
@@ -30,7 +36,7 @@ if __name__ == "__main__":
         Process(target=run_orderbook),
         Process(target=run_price_strategy),
         Process(target=run_news_strategy),        
-        # Process(target=run_ordermanager)
+        Process(target=run_ordermanager)
     ]
     for p in processes: p.start()
     for p in processes: p.join()
