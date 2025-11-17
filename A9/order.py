13e4243fba8b0e362b8e2c254c4de589b90c32bd
Wan <ncvasa88@naver.com>
2025-11-17 13:30:53 -0600

    Add Order and OrderState classes
    Implement Order and OrderState classes for order management.

---
+++ A9/order.py
@@ -0,0 +1,34 @@
+from enum import Enum, auto
+
+class OrderState(Enum): 
+    NEW = auto() # 1st status
+    ACKED = auto() # 2nd status
+    FILLED = auto() # 3rd status
+    CANCELED = auto() # 3rd status
+    REJECTED = auto() # 2nd status
+
+class Order:
+    def __init__(self, symbol, qty, side):
+        self.state = OrderState.NEW
+        self.symbol = symbol
+        self.qty = qty
+        self.side = side
+        self.state = OrderState.NEW
+
+    def transition(self, new_state):
+        allowed = {
+            OrderState.NEW: {OrderState.ACKED, OrderState.REJECTED},
+            OrderState.ACKED: {OrderState.FILLED, OrderState.CANCELED},
+        }
+
+        if new_state in allowed.get(self.state, {}):
+            print(f"[OK] {self.state.name} → {new_state.name}")
+            self.state = new_state
+        else:
+            print(f"[ERROR] Invalid transition: {self.state.name} → {new_state.name}")
+    
+
+state = OrderState.NEW
+
+if state == OrderState.ACKED:
+    print("Order acknowledged")
