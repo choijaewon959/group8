45c3873d9802a0b303044098f4961243cf828d3e
Wan <ncvasa88@naver.com>
2025-11-23 11:30:57 -0600

    Fix import statement for OrderBook module

--- Project-end-to-end-trading-system/gateway.py
+++ Project-end-to-end-trading-system/gateway.py
@@ -1,7 +1,7 @@
 import json
 import time
 from order_manager import OrderManager
-from orderbook2 import OrderBook
+from order_book2 import OrderBook
 
 class Gateway:
     def __init__(self, log_file="./logs/orders.log"):
